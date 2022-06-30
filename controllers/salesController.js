35cd7afa4694723ff68276c9a787ae848aa8e282
lucas_almeida <lucas.almdiniz@gmail.com>
2022-06-30 16:12:08 -0300

    feat: Requisito 8 - criada camada Controller das rotas "GET /sales" e "GET /sales/:id"

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -11,6 +11,24 @@ const addSales = async (req, res, next) => {
   res.status(httpStatus.created).json(sale);
 };
 
+const getAll = async (_req, res) => {
+  const sales = await salesService.getAll();
+
+  res.status(httpStatus.ok).json(sales);
+};
+
+const getById = async (req, res, next) => {
+  const { id: saleId } = req.params;
+
+  const sale = await salesService.getById(saleId);
+
+  if (sale.error) return next(sale.error);
+
+  res.status(httpStatus.ok).json(sale);
+};
+
 module.exports = {
   addSales,
+  getAll,
+  getById,
 };

1b6d6ca6150b7aadb33e9f3c380238c2c1b211cf
lucas_almeida <lucas.almdiniz@gmail.com>
2022-06-30 18:07:02 -0300

    feat: Requisito 10 - criada rota "PUT /products/:id"

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/', rescue(productsController.getAll));
 router.get('/:id', rescue(productsController.getById));
 router.post('/', rescue(productsController.addProduct));
+router.put('/:id', rescue(productsController.updateProduct));
 
 module.exports = router;

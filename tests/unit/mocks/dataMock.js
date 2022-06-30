c57bf0305ca912a066c6fa19c3da3bf16daa09e2
lucas_almeida <lucas.almdiniz@gmail.com>
2022-06-30 17:38:26 -0300

    feat: criado arquivo de mocks para alguns testes

---
+++ tests/unit/mocks/dataMock.js
@@ -0,0 +1,38 @@
+const salesList = [
+  {
+    "saleId": 1,
+    "date": "2021-09-09T04:54:29.000Z",
+    "productId": 1,
+    "quantity": 2
+  },
+  {
+    "saleId": 1,
+    "date": "2021-09-09T04:54:54.000Z",
+    "productId": 2,
+    "quantity": 2
+  },
+  {
+    "saleId": 2,
+    "date": "2022-06-30T19:13:51.000Z",
+    "productId": 3,
+    "quantity": 15
+  }
+];
+
+const idOneSaleList = [
+  {
+    "date": "2021-09-09T04:54:29.000Z",
+    "productId": 1,
+    "quantity": 2
+  },
+  {
+    "date": "2021-09-09T04:54:54.000Z",
+    "productId": 2,
+    "quantity": 2
+  }
+];
+
+module.exports = {
+  salesList,
+  idOneSaleList,
+}

e44bc6153fae732651b5ff663ad8df8cf2e17743
lucas_almeida <lucas.almdiniz@gmail.com>
2022-06-30 17:40:34 -0300

    test: Requisito 9 - criado testes para a camada Model das rotas "GET /sales e GET /sales/:id

--- tests/unit/models/salesModel.test.js
+++ tests/unit/models/salesModel.test.js
@@ -4,8 +4,10 @@ const { execute, expect } = require('chai');
 const salesModel = require('../../../models/salesModel');
 const connection = require('../../../helpers/connection');
 const dataMock = require('../../../__tests__/_dataMock');
+const myDataMock = require('../mocks/dataMock');
 
 const { saleCreateResponse, rightSaleBody } = dataMock;
+const { salesList, idOneSaleList } = myDataMock;
 
 describe('Model - Testes da rota "POST /sales"', () => {
 
@@ -49,4 +51,66 @@ describe('Model - Testes da rota "POST /sales"', () => {
 
   });
 
+});
+
+describe('Model - Testes da rota "GET /sales"', () => {
+
+  describe('quando realiza leitura das vendas', () => {
+
+    beforeEach(() => {
+      sinon.stub(connection, 'execute').resolves([salesList]);
+    });
+
+    afterEach(() => {
+      connection.execute.restore();
+    });
+
+    it('retorna um array de objetos com informações das vendas', async () => {
+      const sales = await salesModel.getAll();
+
+      expect(sales).to.be.eqls(salesList);
+    });
+  });
+});
+
+describe('Model - Testes da rota "GET /sales/:id"', () => {
+
+  describe('quando realiza leitura da venda', () => {
+
+    describe('quando nao encontra a venda', () => {
+      const nonExistentId = 9999;
+      
+      beforeEach(() => {
+        sinon.stub(connection, 'execute').resolves([[]]);
+      });
+
+      afterEach(() => {
+        connection.execute.restore();
+      });
+
+      it('retorna "undefined"', async () => {
+        const sale = await salesModel.getById(nonExistentId);
+
+        expect(sale).to.be.undefined;
+      });
+    });
+
+    describe('quando encontra a venda', () => {
+      const saleId = 1;
+
+      beforeEach(() => {
+        sinon.stub(connection, 'execute').resolves([idOneSaleList]);
+      });
+  
+      afterEach(() => {
+        connection.execute.restore();
+      });
+  
+      it('retorna um array de objetos com informações da venda', async () => {
+        const sale = await salesModel.getById(saleId);
+  
+        expect(sale).to.be.eqls(idOneSaleList);
+      });
+    })
+  });
 });

daf8281a5a419c8079b73daf90f54c4c7d7fcc45
lucas_almeida <lucas.almdiniz@gmail.com>
2022-06-30 23:02:42 -0300

    test: Requisito 11- criado testes para a camada Service da rota "PUT /products/:id"

--- tests/unit/services/productsServices.test.js
+++ tests/unit/services/productsServices.test.js
@@ -4,11 +4,21 @@ const { expect } = require('chai');
 const productsService = require('../../../services/productsService');
 const productsModel = require('../../../models/productsModel');
 const dataMock = require('../../../__tests__/_dataMock');
+const generateError = require('../../../helpers/generateError');
+
+const {
+  allProductsResponse,
+  productCreateResponse,
+  wrongProductBody,
+  rightProductBody,
+  wrongSizeProductBody,
+  productUpdateBody,
+} = dataMock;
 
 describe('Service - Testes da rota "GET /products"', () => {
   describe('quando realiza a leitura de todos os produtos com sucesso', () => {
     before(() => {
-      sinon.stub(productsModel, 'getAll').resolves(dataMock.allProductsResponse);
+      sinon.stub(productsModel, 'getAll').resolves(allProductsResponse);
     });
 
     after(() => {
@@ -20,7 +30,7 @@ describe('Service - Testes da rota "GET /products"', () => {
       it('retorna um array com todos os produtos', async () => {
         const products = await productsService.getAll();
   
-        expect(products).to.be.equals(dataMock.allProductsResponse);
+        expect(products).to.be.equals(allProductsResponse);
       });
     });
   });
@@ -34,7 +44,7 @@ describe('Service - Testes da rota "GET /products/:id"', () => {
       const id = 1;
 
       beforeEach(() => {
-        sinon.stub(productsModel, 'getById').resolves(dataMock.allProductsResponse[0]);
+        sinon.stub(productsModel, 'getById').resolves(allProductsResponse[0]);
       });
 
       afterEach(() => {
@@ -44,7 +54,7 @@ describe('Service - Testes da rota "GET /products/:id"', () => {
       it('retorna um objeto com os dados do produto', async () => {
         const product = await productsService.getById(id);
   
-        expect(product).to.be.equals(dataMock.allProductsResponse[0]);
+        expect(product).to.be.equals(allProductsResponse[0]);
       });
     });
 
@@ -81,7 +91,7 @@ describe('Service - Testes da rota "POST /products"', () => {
       const erro = { error: { code: 'badRequest', message: '"name" is required' } };
 
       it('retorna um error object adequado', async () => {
-        const product = await productsService.addProduct(dataMock.wrongProductBody.name);
+        const product = await productsService.addProduct(wrongProductBody.name);
 
         expect(product).to.be.eqls(erro);
       });
@@ -96,7 +106,7 @@ describe('Service - Testes da rota "POST /products"', () => {
       };
 
       it('retorna um error object adequado', async () => {
-        const product = await productsService.addProduct(dataMock.wrongSizeProductBody.name);
+        const product = await productsService.addProduct(wrongSizeProductBody.name);
 
         expect(product).to.be.eqls(erro);
       });
@@ -106,7 +116,7 @@ describe('Service - Testes da rota "POST /products"', () => {
   describe('quando o "name" é válido', () => {
     describe('quando realiza a inserção do produto', () => {
       beforeEach(() => {
-        sinon.stub(productsModel, 'addProduct').resolves(dataMock.productCreateResponse);
+        sinon.stub(productsModel, 'addProduct').resolves(productCreateResponse);
       });
 
       afterEach(() => {
@@ -114,10 +124,74 @@ describe('Service - Testes da rota "POST /products"', () => {
       });
   
       it('retorna um objeto com os dados do produto', async () => {
-        const product = await productsService.addProduct(dataMock.rightProductBody.name);
+        const product = await productsService.addProduct(rightProductBody.name);
   
-        expect(product).to.be.eqls(dataMock.productCreateResponse);
+        expect(product).to.be.eqls(productCreateResponse);
       });
+    });
+  });
+});
+
+describe('Service - Testes da rota "PUT /products/:id"', () => {
+  const { id, name } = allProductsResponse[0];
+
+  describe('quando o "name" é inválido', () => {
+
+    describe('quando o "name" não existe', () => {
+      const erro = generateError('badRequest', '"name" is required');
+
+      it('deve retornar o error object "{ error: { code: "badRequest", message: ""name" is required" } }"', async () => {
+        const product = await productsService.updateProduct(id, null);
+
+        expect(product).to.be.eqls(erro);
+      });
+    });
+
+    describe('quando o "name" possui comprimento não permitido', () => {
+      const erro = generateError('unprocessableEntity', '"name" length must be at least 5 characters long');
+
+      it('deve retornar o error object "{ error: { code: "unprocessableEntity", message: ""name" length must be at least 5 characters long" } }"', async () => {
+        const product = await productsService.updateProduct(id, wrongSizeProductBody.name);
+
+        expect(product).to.be.eqls(erro);
+      });
+    });
+  });
+
+  describe('quando o produto não existe no banco de dados', () => {
+    const erro = generateError('notFound', 'Product not found');
+
+    beforeEach(() => {
+      sinon.stub(productsModel, 'getById').resolves(undefined);
+    });
+
+    afterEach(() => {
+      productsModel.getById.restore();
+    });
+
+    it('retorna o error object "{ error: code: "notFound", message: "Product not found" }"', async () => {
+      const product = await productsService.updateProduct(9999, productUpdateBody.name);
+
+      expect(product).to.be.eqls(erro);
+    });
+  })
+
+  describe('quando atualiza um product', () => {
+
+    beforeEach(() => {
+      sinon.stub(productsModel, 'getById').resolves(allProductsResponse[0]);
+      sinon.stub(productsModel, 'updateProduct').resolves();
+    });
+
+    afterEach(() => {
+      productsModel.getById.restore();
+      productsModel.updateProduct.restore();
+    });
+
+    it('retorna um objeto contendo informações do produto alterado', async () => {
+      const product = await productsService.updateProduct(id, productUpdateBody.name);
+
+      expect(product).to.be.eqls({ id, name: productUpdateBody.name });
     });
   });
 });

48a2cc376af00b13b2fc732714a19d7aaeec0a94
lucas_almeida <lucas.almdiniz@gmail.com>
2022-06-30 23:03:40 -0300

    test: Requisito 11- criado testes para a camada Controller da rota "PUT /products/:id"

--- tests/unit/controllers/productsController.test.js
+++ tests/unit/controllers/productsController.test.js
@@ -4,6 +4,16 @@ const { expect } = require('chai');
 const productsController = require('../../../controllers/productsController');;
 const productsService = require('../../../services/productsService');
 const dataMock = require('../../../__tests__/_dataMock');
+const generateError = require('../../../helpers/generateError');
+
+const {
+  allProductsResponse,
+  productCreateResponse,
+  rightProductBody,
+  wrongProductBody,
+  wrongSizeProductBody,
+  productUpdateBody,
+} = dataMock;
 
 describe('Controller - Testes da rota "/products"', () => {
   const request = {};
@@ -17,7 +27,7 @@ describe('Controller - Testes da rota "/products"', () => {
   describe('quando realiza a leitura de todos os produtos com sucesso', () => {
 
     before(() => {
-      sinon.stub(productsService, 'getAll').resolves(dataMock.allProductsResponse);
+      sinon.stub(productsService, 'getAll').resolves(allProductsResponse);
     });
 
     after(() => {
@@ -35,7 +45,7 @@ describe('Controller - Testes da rota "/products"', () => {
       it('responde com um json contendo um array com os dados dos produtos', async () => {
         await productsController.getAll(request, response);
   
-        expect(response.json.calledWith(dataMock.allProductsResponse)).to.be.true;
+        expect(response.json.calledWith(allProductsResponse)).to.be.true;
       });
     });
   });
@@ -58,7 +68,7 @@ describe('Controller - Testes da rota "/products/:id"', () => {
 
       beforeEach(() => {
         request.params = { id };
-        sinon.stub(productsService, 'getById').resolves(dataMock.allProductsResponse[0]);
+        sinon.stub(productsService, 'getById').resolves(allProductsResponse[0]);
       });
 
       afterEach(() => {
@@ -74,7 +84,7 @@ describe('Controller - Testes da rota "/products/:id"', () => {
       it('responde com um json contendo os dados do produto', async () => {
         await productsController.getById(request, response);
   
-        expect(response.json.calledWith(dataMock.allProductsResponse[0])).to.be.true;
+        expect(response.json.calledWith(allProductsResponse[0])).to.be.true;
       });
     });
 
@@ -122,7 +132,7 @@ describe('Controller - Testes da rota "POST /products"', () => {
       const erro = { error: { code: 'badRequest', message: '"name" is required' } };
 
       beforeEach(() => {
-        request.body = dataMock.wrongProductBody;
+        request.body = wrongProductBody;
         sinon.stub(productsService, 'addProduct').resolves(erro);
       });
 
@@ -146,7 +156,7 @@ describe('Controller - Testes da rota "POST /products"', () => {
       };
 
       beforeEach(() => {
-        request.body = dataMock.wrongSizeProductBody;
+        request.body = wrongSizeProductBody;
         sinon.stub(productsService, 'addProduct').resolves(erro);
       });
 
@@ -165,8 +175,8 @@ describe('Controller - Testes da rota "POST /products"', () => {
   describe('quando o "name" é válido', () => {
     describe('quando realiza a inserção do produto', () => {
       beforeEach(() => {
-        request.body = dataMock.rightProductBody
-        sinon.stub(productsService, 'addProduct').resolves(dataMock.productCreateResponse);
+        request.body = rightProductBody;
+        sinon.stub(productsService, 'addProduct').resolves(productCreateResponse);
       });
 
       afterEach(() => {
@@ -182,8 +192,97 @@ describe('Controller - Testes da rota "POST /products"', () => {
       it('responde com um json contendo os dados do produto', async () => {
         await productsController.addProduct(request, response);
   
-        expect(response.json.calledWith(dataMock.productCreateResponse)).to.be.true;
+        expect(response.json.calledWith(productCreateResponse)).to.be.true;
       });
+    });
+  });
+});
+
+describe('Controller - Testes da rota "PUT /products/:id"', () => {
+  const request = {};
+  const response = {};
+  let next;
+
+  const { id } = allProductsResponse[0];
+
+  before(() => {
+    response.status = sinon.stub().returns(response);
+    response.json = sinon.stub().returns();
+    next = sinon.stub().returns();
+  })
+
+  describe('quando o "name" é inválido', () => {
+    request.params = id;
+
+    describe('quando o "name" não existe', () => {
+
+      const erro = generateError('badRequest', '"name" is required');
+
+      it('deve retornar a função next contendo como parâmetro o error object "{ code: "badRequest", message: ""name" is required" }"', async () => {
+        request.body = wrongProductBody;
+        
+        await productsController.updateProduct(request, response, next);
+
+        expect(next.calledWith(erro.error)).to.be.true;
+      });
+    });
+
+    describe('quando o "name" possui comprimento não permitido', () => {
+      const erro = generateError('unprocessableEntity', '"name" length must be at least 5 characters long');
+
+      it('deve retornar a função next contendo como parâmetro o error object "{ code: "unprocessableEntity", message: ""name" length must be at least 5 characters long" }"', async () => {
+        request.body = wrongSizeProductBody;
+
+        await productsController.updateProduct(request, response, next);
+
+        expect(next.calledWith(erro.error)).to.be.true;
+      });
+    });
+  });
+
+  describe('quando o produto não existe no banco de dados', () => {
+    const erro = generateError('notFound', 'Product not found');
+
+    beforeEach(() => {
+      sinon.stub(productsService, 'updateProduct').resolves(erro);
+    });
+
+    afterEach(() => {
+      productsService.updateProduct.restore();
+    });
+
+    it('deve retornar a função next contendo como parâmetro o error object "{ code: "notFound", message: "Product not found" }"', async () => {
+      request.params = { id: 9999 };
+      request.body = productUpdateBody;
+
+      await productsController.updateProduct(request, response, next);
+
+      expect(next.calledWith(erro.error)).to.be.true;
+    });
+  })
+
+  describe('quando atualiza um product', () => {
+    request.params = { id: allProductsResponse[0].id };
+    request.body = productUpdateBody;
+
+    beforeEach(() => {
+      sinon.stub(productsService, 'updateProduct').resolves({ id, name: productUpdateBody.name });
+    });
+
+    afterEach(() => {
+      productsService.updateProduct.restore();
+    });
+
+    it('responde com status "200"', async () => {
+      await productsController.updateProduct(request, response, next);
+
+      expect(response.status.calledWith(200)).to.be.true;
+    });
+
+    it('responde com um json contendo um objeto com informações do produto atualizado', async () => {
+      await productsController.updateProduct(request, response, next);
+
+      expect(response.json.calledWith({ id, name: productUpdateBody.name })).to.be.true;
     });
   });
 });

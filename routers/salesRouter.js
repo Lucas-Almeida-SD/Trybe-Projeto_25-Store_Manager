63354195e03f8737f6812785c81413463ef040d1
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-01 00:27:37 -0300

    feat: Requisito 14 - criada rota "DELETE /sales/:id"

--- routers/salesRouter.js
+++ routers/salesRouter.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/', rescue(salesController.addSales));
 router.get('/', rescue(salesController.getAll));
 router.get('/:id', rescue(salesController.getById));
+router.delete('/:id', rescue(salesController.deleteSales));
 
 module.exports = router;

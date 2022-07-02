d56916caf469339995614d662c4e2a51714ad40a
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-02 19:11:04 -0300

    feat: Requisito 18 - criada rota "GET /products/search?q="

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -5,6 +5,7 @@ const productsController = require('../controllers/productsController');
 const router = express.Router();
 
 router.get('/', rescue(productsController.getAll));
+router.get('/search', rescue(productsController.searchProduct));
 router.get('/:id', rescue(productsController.getById));
 router.post('/', rescue(productsController.addProduct));
 router.put('/:id', rescue(productsController.updateProduct));

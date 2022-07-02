30daa0b5ec432313112042126ef88245f2913321
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-01 22:06:56 -0300

    feat: Requisito 16 - criada camada Controller da rota "PUT /sales/:id"

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -37,9 +37,22 @@ const deleteSales = async (req, res, next) => {
   res.status(httpStatus.noContent).end();
 };
 
+const updateSalesProducts = async (req, res, next) => {
+  const { id: saleId } = req.params;
+  const salesProducts = req.body;
+
+  const updatedSalesProducts = await salesService
+    .updateSalesProducts(Number(saleId), salesProducts);
+
+  if (updatedSalesProducts.error) return next(updatedSalesProducts.error);
+
+  res.status(httpStatus.ok).json(updatedSalesProducts);
+};
+
 module.exports = {
   addSales,
   getAll,
   getById,
   deleteSales,
+  updateSalesProducts,
 };

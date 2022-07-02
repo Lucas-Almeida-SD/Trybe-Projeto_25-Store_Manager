c4bac23e9131a44815f9a2aa6a15b6cac355ee23
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-02 00:50:43 -0300

    test: Requisito 17 - criado testes para camada Controller da rota "PUT /sales/:id"

--- tests/unit/controllers/salesController.test.js
+++ tests/unit/controllers/salesController.test.js
@@ -13,6 +13,8 @@ const {
   wrongZeroNegativeBody,
   wrongZeroQuantityBody,
   saleCreateResponse,
+  rightSaleBody,
+  nonexistentProductIdBody,
 } = dataMock;
 
 const { salesList, idOneSaleList } = myDataMock;
@@ -262,6 +264,166 @@ describe('Controller - Testes da rota "DELETE /sales/:id"', () => {
       await salesController.deleteSales(request, response, next);
 
       expect(response.status.calledWith(204)).to.be.true;
+    });
+  });
+});
+
+describe('Controller - Testes da rota "PUT /sales/:id"', () => {
+  const request = {};
+  const response = {};
+  let next;
+
+  const saleId = 1;
+  const nonExistentId = 9999;
+
+  before(() => {
+    response.status = sinon.stub().returns(response);
+    response.json = sinon.stub().returns();
+    next = sinon.stub().returns();
+  });
+
+  describe('quando "sales products" é inválido', () => {
+
+    describe('quando algum "productId" não existe', () => {
+      const erro = generateError('badRequest', '"productId" is required');
+
+      beforeEach(() => {
+        sinon.stub(salesService, 'updateSalesProducts').resolves(erro);
+      });
+
+      afterEach(() => {
+        salesService.updateSalesProducts.restore();
+      });
+
+      it('retorna a função next contendo como parâmetro o error object "{ code: "badRequest", message: ""productId" is required" }"', async () => {
+        request.params = saleId;
+        response.body = wrongSaleNotProductIdBody;
+          
+        await salesController.updateSalesProducts(request, response, next);
+
+        expect(next.calledWith(erro.error)).to.be.true;
+      });
+    });
+
+    describe('quando algum "quantity" não existe', () => {
+      const erro = generateError('badRequest', '"quantity" is required');
+
+      beforeEach(() => {
+        sinon.stub(salesService, 'updateSalesProducts').resolves(erro);
+      });
+
+      afterEach(() => {
+        salesService.updateSalesProducts.restore();
+      });
+
+      it('retorna a função next contendo como parâmetro o error object "{ code: "badRequest", message: ""quantity" is required" }"', async () => {
+        request.params = saleId;
+        response.body = wrongSaleNotQuantityBody;
+          
+        await salesController.updateSalesProducts(request, response, next);
+
+        expect(next.calledWith(erro.error)).to.be.true;
+      });
+    });
+
+    describe('quando algum "quantity" é menor ou igual "0"', () => {
+      const erro = generateError('unprocessableEntity', '"quantity" must be greater than or equal to 1');
+
+      beforeEach(() => {
+        sinon.stub(salesService, 'updateSalesProducts').resolves(erro);
+      });
+
+      afterEach(() => {
+        salesService.updateSalesProducts.restore();
+      });
+
+      it('menor que "0" - retorna a função next contendo como parâmetro o error object "{ code: "unprocessableEntity", message: ""quantity" must be greater than or equal to 1" }"', async () => {
+        request.params = saleId;
+        response.body = wrongZeroNegativeBody;
+          
+        await salesController.updateSalesProducts(request, response, next);
+
+        expect(next.calledWith(erro.error)).to.be.true;
+      });
+
+      it('igual a "0" - retorna a função next contendo como parâmetro o error object "{ code: "unprocessableEntity", message: ""quantity" must be greater than or equal to 1" }"', async () => {
+        request.params = saleId;
+        response.body = wrongZeroQuantityBody;
+          
+        await salesController.updateSalesProducts(request, response, next);
+
+        expect(next.calledWith(erro.error)).to.be.true;
+      });
+    });
+  });
+
+  describe('quando "sale" não existe no banco de dados', () => {
+    const erro = generateError('notFound', 'Sale not found');
+
+    beforeEach(() => {
+      sinon.stub(salesService, 'updateSalesProducts').resolves(erro);
+    });
+
+    afterEach(() => {
+      salesService.updateSalesProducts.restore();
+    });
+
+    it('retorna a função next contendo como parâmetro o error object "{ code: "notFound", message: "Sale not found" }', async () => {
+      request.params = nonExistentId;
+      response.body = rightSaleBody;
+        
+      await salesController.updateSalesProducts(request, response, next);
+
+      expect(next.calledWith(erro.error)).to.be.true;
+    });
+  });
+
+  describe('quando algum "product" não existe no banco de dados', () => {
+    const erro = generateError('notFound', 'Product not found');
+
+    beforeEach(() => {
+      sinon.stub(salesService, 'updateSalesProducts').resolves(erro);
+    });
+
+    afterEach(() => {
+      salesService.updateSalesProducts.restore();
+    });
+
+    it('retorna a função next contendo como parâmetro o error object "{ code: "notFound", message: "Product not found" }', async () => {
+      request.params = saleId;
+      response.body = nonexistentProductIdBody;
+        
+      await salesController.updateSalesProducts(request, response, next);
+
+      expect(next.calledWith(erro.error)).to.be.true;
+    });
+  });
+
+  describe('quando atualiza os "sales products"', () => {
+    const updatedSalesProducts = { saleId, itemsUpdated: rightSaleBody }
+
+    beforeEach(() => {
+      request.params = saleId;
+      response.body = rightSaleBody;
+
+      sinon.stub(salesService, 'updateSalesProducts').resolves(updatedSalesProducts);
+    });
+
+    afterEach(() => {
+      salesService.updateSalesProducts.restore();
+    });
+
+    it('responde com status "200"', async () => {
+      await salesController.updateSalesProducts(request, response, next);
+
+      expect(response.status.calledWith(200)).to.be.true;
+    });
+
+
+    it('responde com status "200"', async () => {
+      await salesController.updateSalesProducts(request, response, next);
+
+      expect(response.json.calledWith(updatedSalesProducts)).to.be.true;
     });
   });
 });

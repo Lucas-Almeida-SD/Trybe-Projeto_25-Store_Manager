f1476069669773fe9f087e2cdc65da0e43f861fe
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-05 14:09:18 -0300

    chore: organizando posição das rotas

--- routers/salesRouter.js
+++ routers/salesRouter.js
@@ -4,10 +4,10 @@ const salesController = require('../controllers/salesController');
 
 const router = express.Router();
 
-router.post('/', rescue(salesController.addSales));
 router.get('/', rescue(salesController.getAll));
 router.get('/:id', rescue(salesController.getById));
-router.delete('/:id', rescue(salesController.deleteSales));
+router.post('/', rescue(salesController.addSales));
 router.put('/:id', rescue(salesController.updateSalesProducts));
+router.delete('/:id', rescue(salesController.deleteSales));
 
 module.exports = router;

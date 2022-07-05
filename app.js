dc50068cfeb11155b38f0218fb99957c9a33a750
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-05 14:10:32 -0300

    chore: adicionada rota para documentação

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const productsRouter = require('./routers/productsRouter');
 const salesRouter = require('./routers/salesRouter');
 const errorMiddleware = require('./middlewares/errorMiddleware');
+const swaggerFile = require('./swagger_output.json');
 
 const app = express();
 app.use(express.json());
 
-// não remova esse endpoint, é para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
-});
-
 app.use('/products', productsRouter);
 app.use('/sales', salesRouter);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(errorMiddleware);
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
 module.exports = app;

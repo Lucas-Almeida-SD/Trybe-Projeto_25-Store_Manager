0263f982bf9b53f3d9958fc36301d903d7550678
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-05 23:49:18 -0300

    feat: responde com um erro genérico para erros diversos

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,9 +1,13 @@
 const httpStatus = require('../helpers/httpStatus');
 
 const errorMiddleware = (err, req, res, _next) => {
-  const { code, message } = err;
+  if (err.code && err.message) {
+    const { code, message } = err;
+    return res.status(httpStatus[code]).json({ message });
+  }
 
-  res.status(httpStatus[code]).json({ message });
+  const message = 'Internal Server Error';
+  res.status(httpStatus.internalServerError).json({ message });
 };
 
 module.exports = errorMiddleware;
